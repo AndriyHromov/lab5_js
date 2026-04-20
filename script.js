d97dd4a6ba93d9ee5c9c850a49e65f6b193730f6
AndriyHromov <146163843+AndriyHromov@users.noreply.github.com>
2026-04-20 12:08:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-function showText() {
-    let input = document.getElementById("userInput").value;
+let box = document.getElementById("box");
+let score = 0;
 
-    if (input === "") {
-        document.getElementById("output").textContent = "Ви нічого не ввели!";
-    } else {
-        document.getElementById("output").textContent = "Ви ввели: " + input;
-    }
+function spawnBox() {
+    let gameArea = document.getElementById("gameArea");
+
+    let maxX = gameArea.clientWidth - 50;
+    let maxY = gameArea.clientHeight - 50;
+
+    let x = Math.random() * maxX;
+    let y = Math.random() * maxY;
+
+    box.style.left = x + "px";
+    box.style.top = y + "px";
+
+    let color = document.getElementById("colorPicker").value;
+    box.style.backgroundColor = color;
+
+    box.style.display = "block";
+
+    let time = document.getElementById("difficulty").value;
+
+    setTimeout(() => {
+        box.style.display = "none";
+        spawnBox();
+    }, time);
 }
+
+box.onclick = function () {
+    score++;
+    document.getElementById("score").textContent = score;
+    box.style.display = "none";
+    spawnBox();
+};
+
+spawnBox();
